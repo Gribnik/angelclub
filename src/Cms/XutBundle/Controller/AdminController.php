b92063f51525da1094c23d5a3efbb7ca8235a340
enarc <enarc@atwix.com>
2013-09-05 18:34:43 +0300

    Added auth logic for admin

---
+++ src/Cms/XutBundle/Controller/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cms\XutBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AdminController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('CmsXutBundle:Admin:index.html.twig');
+    }
+}
