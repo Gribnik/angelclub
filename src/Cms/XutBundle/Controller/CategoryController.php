52a11ca8721f3fd6828172fc058087f8d93fcb91
Gribnik <rogyar@gmail.com>
2014-06-22 12:01:13 +0300

    Ability to edit categories names on the fly

--- src/Cms/XutBundle/Controller/CategoryController.php
+++ src/Cms/XutBundle/Controller/CategoryController.php
@@ -32,7 +32,7 @@ class CategoryController extends Controller
             if (count($categories) > 0) {
                 foreach ($categories as $_category) {
                     $forms[] = array(
-                        'formview' => $this->createForm(new CategoryType(), $_category)->createView(),
+                        'formview' => $this->createForm(new CategoryType(), $_category, array('method' => 'PUT'))->createView(),
                         'category' => $_category
                     );
                 }
@@ -52,8 +52,17 @@ class CategoryController extends Controller
 
     public function saveAction($category_id = 0)
     {
+        $putJson = file_get_contents('php://input');
+        $parameters = (array) json_decode($putJson);
+
+        /* There are some problems using form->put->request flow with backbone. So, we have some dog-nails here */
+        
+        $necessaryParameters = array('name' => '', '_token' => '');
+        $parameters = array_intersect_key($parameters, $necessaryParameters);
         $request = $this->getRequest();
-        if ($request->getMethod() == 'POST' && $this->_isAdmin()) {
+        if ($request->getMethod() == 'PUT' && $this->_isAdmin()) {
+            $requestToHandle = $request->create($request->getUri(), 'PUT',
+                array('category' => (array) $parameters));
             $em = $this->getDoctrine()->getManager();
             if (!$category_id) {
                 $category = new Category();
@@ -65,13 +74,14 @@ class CategoryController extends Controller
                 }
             }
 
-            $form = $this->createForm(new CategoryType(), $category);
-            $form->handleRequest($request);
+            $form = $this->createForm(new CategoryType(), $category, array('method' => 'PUT'));
+            $form->handleRequest($requestToHandle);
             if ($form->isValid()) {
                 $em->persist($category);
                 $em->flush();
             } else {
-                return $this->get('backpack')->sendJsonResponseText('The form has missing required fields', 'error');
+                return $this->get('backpack')->sendJsonResponseText('The form has missing required fields' . $form->getErrorsAsString(),
+                    'error');
             }
         } else {
             throw new AccessDeniedException();
